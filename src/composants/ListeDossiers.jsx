6086a90cdf91ad2c2739750d0896c1f65e9e6aea
Camille Semaan <cdem@xvitae.com>
2021-03-22 14:08:25 -0400

    Petite correction

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -4,13 +4,9 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore';
 import { useState, useEffect } from 'react';
 
+// Votre propre objet de configuration ici ;-)
 const firebaseConfig = {
-  apiKey: "AIzaSyCoWgkBcWdz_M9GjEr5TE-b7tJfejQy1JI",
-  authDomain: "react-iwra.firebaseapp.com",
-  projectId: "react-iwra",
-  storageBucket: "react-iwra.appspot.com",
-  messagingSenderId: "422100533764",
-  appId: "1:422100533764:web:5cb055c9062799e8812b51"
+  
 };
 
 // Étape A : Initialiser Firebase
