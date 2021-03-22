b930cd327f76a674d96e5c9b093d30330905d59f
Camille Semaan <cdem@xvitae.com>
2021-03-22 13:43:39 -0400

    Intégration Firestore - code incomplet

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -2,18 +2,18 @@ import './Dossier.scss';
 import { IconButton } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-export default function Dossier({id, titre, couleur, dateModif}) {
+export default function Dossier({nom, couleur, modification, image}) {
   return (
     <article className="Dossier" style={{backgroundColor: couleur}}>
       <div className="couverture">
         <IconButton className="deplacer" aria-label="déplacer" disableRipple={true}>
           <SortIcon />
         </IconButton>
-        <img src={`images-dossiers/${id}.png`} alt={titre}/>
+        <img src={image} alt={nom}/>
       </div>
       <div className="info">
-        <h2>{titre}</h2>
-        <p>Modifié : {dateModif}</p>
+        <h2>{nom}</h2>
+        {/* <p>Modifié : {modification}</p> */}
       </div>
       <IconButton className="modifier" aria-label="modifier" size="small">
         <MoreVertIcon />
--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -1,18 +1,47 @@
 import './ListeDossiers.scss';
-import dossTab from '../data/liste-dossiers.json';
 import Dossier from './Dossier';
 import firebase from 'firebase/app';
 import firestore from 'firebase/firestore';
+import { useState, useEffect } from 'react';
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCoWgkBcWdz_M9GjEr5TE-b7tJfejQy1JI",
+  authDomain: "react-iwra.firebaseapp.com",
+  projectId: "react-iwra",
+  storageBucket: "react-iwra.appspot.com",
+  messagingSenderId: "422100533764",
+  appId: "1:422100533764:web:5cb055c9062799e8812b51"
 };
 
+// Étape A : Initialiser Firebase
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Étape B : Initialiser Firestore
+const dbFirestore = firebase.firestore();
+
+
+
 export default function ListeDossiers() {
+  const [dossiers, setDossiers] = useState([]);
+
+  // Étape C : Exécuter une requête sur la collection 'dossiers-temp' pour lire l'info des dossiers disponibles
+  // Comme le code suivant fait appel à une ressource externe, il est ASYNCHRONE
+  // donc, il n'y a pas de garantie que le tableau dossiers soit remplit avant l'affichage du composant
+  useEffect(
+    () => dbFirestore.collection('dossiers-temp').get().then(
+            reponse => reponse.forEach(
+              doc => dossiers.push(doc.data())
+            )
+          )
+    , []
+  );
+
   return (
     <ul className="ListeDossiers">
       {
-        dossTab.map( 
+        dossiers.map( 
           dossier =>  <li key={dossier.id}><Dossier {...dossier} /></li>
         )
       }
