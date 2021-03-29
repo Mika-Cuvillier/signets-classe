62f11813c9eede497874f81ebe2ed58a18d9f698
Camille Semaan <cdem@xvitae.com>
2021-03-29 12:51:20 -0400

    Petite modif

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,12 +4,7 @@ import * as firebaseui from 'firebaseui';
 
 // Votre propre objet de configuration ici ;-)
 const firebaseConfig = {
-  apiKey: "AIzaSyCoWgkBcWdz_M9GjEr5TE-b7tJfejQy1JI",
-  authDomain: "react-iwra.firebaseapp.com",
-  projectId: "react-iwra",
-  storageBucket: "react-iwra.appspot.com",
-  messagingSenderId: "422100533764",
-  appId: "1:422100533764:web:5cb055c9062799e8812b51"
+  
 };
 
 // Initialiser Firebase
